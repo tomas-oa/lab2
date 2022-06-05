198fd735de7c4872b0637b112203fba83121f45b
Tomás Opazo <78031280+tomas-oa@users.noreply.github.com>
2022-06-05 00:12:35 -0400

    primera parte lab 2
    espero que me pongan un 7

---
+++ Clear.java
@@ -0,0 +1,22 @@
+package Console;
+
+public class Clear {
+    public static void clearConsole() {
+        try{
+            String operatingSystem = System.getProperty("os.name");
+
+            if(operatingSystem.contains("Windows")){
+                ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "cls");
+                Process startProcess = pb.inheritIO().start();
+                startProcess.waitFor();
+            } else {
+                ProcessBuilder pb = new ProcessBuilder("clear");
+                Process startProcess = pb.inheritIO().start();
+
+                startProcess.waitFor();
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ InsertionSort.java
@@ -0,0 +1,23 @@
+package Sorts;
+
+import java.util.*;
+
+public class InsertionSort <T extends Comparable<T>> {
+    public List<T> sort(List<T> list, Comparator<T> comparator) {
+        int i, x = 0;
+        T key;
+
+        for (i = 1; i < list.size(); i++) {
+            key = list.get(i);
+            x = i - 1;
+
+            while (0 <= x && comparator.compare(list.get(x), key) > 0) {
+                list.set(x+1, list.get(x));
+                x--;
+            }
+            list.set(x + 1, key);
+        }
+
+        return new ArrayList<>(list);
+    }
+}
